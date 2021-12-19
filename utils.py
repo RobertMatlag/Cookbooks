0b3c6e9f684d8312ab0a397afadb4a0806729319
Robert Matląg <r.matlag@getprintbox.com>
2021-12-19 13:48:02 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def prepare_file_name(s: str) -> str:
-    return s.lower().replace(' ', '_')
+    return s.lower().replace(".", "").replace(" ", "_")
 
 
 if __name__ == "__main__":
-    print(prepare_file_name('20 Combining Multiple Mappings into a Single Mapping'))
+    print(prepare_file_name("3. Matching Strings Using Shell Wildcard Patterns"))
